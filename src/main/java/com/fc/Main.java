3862c5d52f2f8a217e212ad78c311f62a632d46f
dev <sh7824@naver.com>
2023-01-11 18:32:43 +0900

    Update #3 -Main
    변수 클래스를 인터페이스로 변경
    #3 은 여기서 마무리

--- src/main/java/com/fc/Main.java
+++ src/main/java/com/fc/Main.java
@@ -1,12 +1,14 @@
 package com.fc;
 
 import com.fc.logic.BubbleSort;
+import com.fc.logic.JavaSort;
+import com.fc.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
 
         System.out.println("[result]" + sort.sort(Arrays.asList(args)));
     }
